9caacc9495544f7a75a1b4de48c81ec53096b945
DongIkkk <w8h0412@gmail.com>
2022-09-27 09:53:44 +0900

    day2 logging

--- introduce/views.py
+++ introduce/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import AccessLog
 
 
 # Create your views here.
 def introduce(request):
+    new_log = AccessLog()
+    new_log.location = 'introduce.html'
+    new_log.save()
     return render(request, 'introduce.html')
