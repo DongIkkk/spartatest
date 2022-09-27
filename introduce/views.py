2318bcbc9efb8cc3b97c92b52fe159421121ceda
DongIkkk <w8h0412@gmail.com>
2022-09-27 14:45:05 +0900

    test

--- introduce/views.py
+++ introduce/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import AccessLog
 
 
-# Create your views here.
 def introduce(request):
     new_log = AccessLog()
     new_log.location = 'introduce.html'
